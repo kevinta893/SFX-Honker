04c72a278a47aeb9b184de51bf6f5f549f0a5b02
kevinta893 <kevinta893@gmail.com>
2016-06-28 13:46:44 -0600

    Added Settings activity

--- SFXHonker/app/src/main/java/com/example/android/sfxhonker/MainActivity.java
+++ SFXHonker/app/src/main/java/com/example/android/sfxhonker/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.sfxhonker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -44,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this.getApplicationContext(), SettingsActivity.class);
+            startActivity(settingsIntent);
             return true;
         }
 
